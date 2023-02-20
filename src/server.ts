fdb04a582ac8dcdcd9f6173cc5515e08e62c321b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-02-20 20:07:45 +0800

    refactor

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,12 @@ const PORT = process.env.PORT
 const app = express()
 
 // * listen for requests
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log('Your app is listening on port ' + PORT)
 })
 
 // * middlewares
-app.use(morgan('tiny'))
+app.use(morgan('tiny')) // * logs the request
 app.use(cors({ optionsSuccessStatus: 200 })) // * some legacy browsers choke on 204
 app.use(express.static('public')) // * http://expressjs.com/en/starter/static-files.html
 
